4f4fab84a35fe2cd116e4b22f5ec2e215d6283ee
김태영 <rlaxodud9809@khu.ac.kr>
2020-11-29 00:00:09 +0900

    add print image

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MWD' , url: "/images/1.jpg"});
 });
 
-/* predict() 의 결과를 querystring으로 받아 그것에 맞는 이미지파일을 보내준다. */
+/* predict() 의 결과를 querystring으로 받아 그것에 맞는 이미지와 음성파일을 보내준다. */
 /* /data?id=n 의 형태로 id값을 전달해 준다. */
 router.get('/data', function(req, res, next){
 
   id = req.query.id;
-  console.log(id);
+
+  data = {
+    image : "/images/"+id+".jpg"
+  }
+  //데이터 확인
+  console.log(data);
   //보내주기.
   res.send(data);
 });
