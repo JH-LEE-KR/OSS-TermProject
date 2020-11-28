07f8cfcb11e0c3b1566f06f25163cfc278fe78be
김태영 <rlaxodud9809@khu.ac.kr>
2020-11-28 23:58:40 +0900

    add temp image file & request image address by ajax

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MWD' , url: "/images/1.jpg"});
+});
+
+/* predict() 의 결과를 querystring으로 받아 그것에 맞는 이미지파일을 보내준다. */
+/* /data?id=n 의 형태로 id값을 전달해 준다. */
+router.get('/data', function(req, res, next){
+
+  id = req.query.id;
+  console.log(id);
+  //보내주기.
+  res.send(data);
 });
 
 module.exports = router;
+
